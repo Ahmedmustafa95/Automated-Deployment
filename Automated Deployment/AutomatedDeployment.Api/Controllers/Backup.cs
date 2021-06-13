7edee2776401968bc46e999035474b8fa88ff4bd
Ahmedmustafa95 <Ahmedmustafa2699@gmail.com>
2021-06-13 16:07:07 +0200

    Generate Models from database

---
+++ Automated Deployment/AutomatedDeployment.Api/Controllers/Backup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomatedDeployment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Backup : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult MoveTOBackUpFolder(int HubId,int ApplicationID)
+        {
+            // select  assembly file from connfiguratuion where 
+
+            return Ok();
+        }
+    }
+}
