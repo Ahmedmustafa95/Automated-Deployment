b1f0884687ed38b8a5e8378047495049d2531eba
Ahmed Mahmoud <ahmedmahmoudabdelaty@gmail.com>
2021-06-23 14:01:56 +0200

    String Maniupulation isa finished

--- Automated Deployment/AutomatedDeployment.Api/Services/Interfaces/IBackupServices.cs
+++ Automated Deployment/AutomatedDeployment.Api/Services/Interfaces/IBackupServices.cs
@@ -9,5 +9,7 @@ namespace AutomatedDeployment.Api.Services
     public interface IBackupServices
     {
         void MoveTOBackUpFolder(List<string> filesName, string assemblyPath, string backupPath);
+        void MoveTOBackUpFolder(string fileName, string backupPath);
+
     }
 }
--- Automated Deployment/AutomatedDeployment.Domain/Entities/ConfigSearchResult.cs
+++ Automated Deployment/AutomatedDeployment.Domain/Entities/ConfigSearchResult.cs
@@ -12,10 +12,12 @@ namespace AutomatedDeployment.Domain.Entities
         public int AppID { get; set; }
         public string HubName { get; set; }
         public string AppName { get; set; }
-        public string ConfigurationSectionName { get; set; }
-        public string ConfigurationName { get; set; }
-        public string ConfigurationValue { get; set; }
+        //public string ConfigurationSectionName { get; set; }
+        //public string ConfigurationName { get; set; }
+        //public string ConfigurationValue { get; set; }
         public string FileName { get; set; }
-        public string ConfigurationResult { get; set; }
+        public string NewConfigurationResult { get; set; }
+        public string OldConfigurationResult { get; set; }
+
     }
 }
